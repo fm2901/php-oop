75cfaa62bf8ea7d0cd422560678d104a94ab25fc
Азимов Сухроб <sukhrobazimov1996@gmail.com>
2022-08-31 19:00:49 +0500

    add abstract class Product, extend it and create class Book

--- index.php
+++ index.php
@@ -10,5 +10,33 @@ spl_autoload_register(function ($class_name)
 });
 
 $person = new Person("Abdu", 35);
-$person->printInfo();
-echo $person->name;
+//$person->printInfo();
+//echo $person->name;
+
+//class A {
+//    public static function who() {
+//        echo __CLASS__;
+//    }
+//    public static function test() {
+//        static::who();
+//    }
+//}
+//
+//class B extends A {
+//    public static function who() {
+//        echo __CLASS__;
+//    }
+//}
+//
+//class C extends A {
+//    public static function who() {
+//        echo __CLASS__;
+//    }
+//}
+//
+//B::test();
+//C::test();
+//C::who();
+
+$book = new Book("Чингисхан", 200);
+echo $book->getName();
