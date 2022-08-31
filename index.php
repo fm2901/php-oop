0d38c0554fef38ada25ffe0350497de2c462c9d6
Азимов Сухроб <sukhrobazimov1996@gmail.com>
2022-08-31 18:31:24 +0500

    fix __autoload deprecated fatal error

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 include "configs.php";
 
-function __autoload($class_name)
+spl_autoload_register(function ($class_name)
 {
     require_once(CLASSES_PATH."/".$class_name.'.class.php');
     if(method_exists($class_name,'init'))
         call_user_func(array($class_name,'init'));
     return true;
-}
+});
 
 $person = new Person("Abdu", 35);
 $person->printInfo();
