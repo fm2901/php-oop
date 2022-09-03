9d55acf9a5f11edadeda07d8dd26e8d8cc55949b
Fayziev Muminjon <fayzievmuminjon@gmail.com>
2022-09-03 13:35:47 +0500

    refactoring autoload func by PSR rules

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ include "configs.php";
 spl_autoload_register(function ($class_name)
 {
     require_once(CLASSES_PATH."/".$class_name.'.php');
-    if(method_exists($class_name,'init'))
+    if (method_exists($class_name,'init')) {
         call_user_func(array($class_name,'init'));
+    }
+
     return true;
 });
 
